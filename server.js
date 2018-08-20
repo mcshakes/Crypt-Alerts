6ceb26d07b0d5ca555857891d1e6cda8b4b116c3
Edward McCarthy <efmccarthy26@gmail.com>
2018-08-20 06:42:12 -0600

    More endpoints to compare

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.listen(3001, () => {
   console.log("listening on Port 3000")
 });
 
-const key = provess.env.NOMICS_KEY
+const key = process.env.NOMICS_KEY
 // app.get("https://api.nomics.com/v1/prices?key=${key}", (resp) => {
 //   res.send(data)
 // })
@@ -51,6 +51,27 @@ app.get("/api/bitcoin-price", (req, res) => {
   })
 })
 
+// NOTE: Price by market interval
 app.get("/api/market-interval-btc", (req, res) => {
+  axios.get(`https://api.nomics.com/v1/exchange-markets/interval?key=${key}&currency=BTC&start=2018-04-14T00%3A00%3A00Z&end=2018-07-14T00%3A00%3A00Z`
+    )
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    })
+})
 
+// NOTE: Aggregated OHLC candles
+
+app.get("/api/candles", (req, res) => {
+  axios.get(`https://api.nomics.com/v1/candles?key=${key}&interval=1d&currency=ETH&start=2018-04-14T00%3A00%3A00Z&end=2018-05-14T00%3A00%3A00Z`
+    )
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    })
 })
