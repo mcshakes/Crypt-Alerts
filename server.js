9b629d969ddbfa3f1aab863dbb368c29ee13ec56
Edward McCarthy <efmccarthy26@gmail.com>
2018-08-20 06:06:31 -0600

    removed api key

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const axios = require('axios');
+require("dotenv").config();
 
 const app = express();
 const server = require("http").Server(app);
@@ -13,3 +15,42 @@ app.use(bodyParser.json());
 server.listen(3001, () => {
   console.log("listening on Port 3000")
 });
+
+const key = provess.env.NOMICS_KEY
+// app.get("https://api.nomics.com/v1/prices?key=${key}", (resp) => {
+//   res.send(data)
+// })
+
+
+// NOTE: Specific price
+
+app.get("/api/price", (req, res) => {
+  axios.get(`https://api.nomics.com/v1/prices?key=${key}`)
+    .then((response) => {
+      // res.data is array of objects
+      response.data.find((coin) => {
+        if (coin.currency === "BTC")  {
+          console.log(coin)
+        }
+      })
+    })
+    .catch((error) => {
+      res.status(400).send(err);
+    })
+})
+
+// NOTE: Price by markets
+
+app.get("/api/bitcoin-price", (req, res) => {
+  axios.get(`https://api.nomics.com/v1/markets/prices?key=${key}&currency=BTC`)
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    res.status(400).send(error);
+  })
+})
+
+app.get("/api/market-interval-btc", (req, res) => {
+
+})
