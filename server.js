579561c8851e44f251153b8c29f66d1b2c7529ba
Edward McCarthy <efmccarthy26@gmail.com>
2018-08-21 14:11:26 -0600

    Response is comming in as changes for Aggregated Price Candles

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 const app = express();
 const server = require("http").Server(app);
+
 const io = require("socket.io")(server);
 const bodyParser = require("body-parser");
 
@@ -20,6 +21,21 @@ server.listen(3001, () => {
 
 const key = process.env.NOMICS_KEY
 
+let interval;
+
+io.on("connection", socket => {
+  console.log("New IO Connected ...");
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
 //------------ DATABASE ------------------------
 
 mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env
@@ -27,17 +43,10 @@ mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env
 
 mongoose.Promise = global.Promise;
 
-// db.on('error', err => {
-//     console.error.bind('FAILED to connect to mongoose', err);
-// });
-//
-// db.once('open', () => {
-//     console.log('connected to mongoose');
-// });
-
 // NOTE: Specific price
 
 const capLeaders = ["BTC", "ETH", "XRP", "BCH", "EOS"]
+
 
 app.get("/api/market-leaders", (req, res) => {
   axios.get(`https://api.nomics.com/v1/prices?key=${key}`)
@@ -74,6 +83,7 @@ app.get("/api/market-leaders", (req, res) => {
 // })
 
 // NOTE: Price by market interval
+
 app.get("/api/market-interval-btc", (req, res) => {
   axios.get(`https://api.nomics.com/v1/exchange-markets/interval?key=${key}&currency=BTC&start=2018-04-14T00%3A00%3A00Z&end=2018-07-14T00%3A00%3A00Z`
     )
@@ -88,10 +98,11 @@ app.get("/api/market-interval-btc", (req, res) => {
 // NOTE: Aggregated OHLC candles
 
 app.get("/api/candles", (req, res) => {
+  // res
   axios.get(`https://api.nomics.com/v1/candles?key=${key}&interval=1d&currency=ETH&start=2018-04-14T00%3A00%3A00Z&end=2018-05-14T00%3A00%3A00Z`
     )
     .then((response) => {
-      console.log(response)
+      res.json(response.data)
     })
     .catch((error) => {
       res.status(400).send(error);
