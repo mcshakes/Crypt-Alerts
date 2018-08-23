92cab9b15ef729cce9a7d9e3a82767d6952f0149
Edward McCarthy <efmccarthy26@gmail.com>
2018-08-22 23:24:21 -0600

    Forgot the return

--- client/src/components/SearchSuggestions.js
+++ client/src/components/SearchSuggestions.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchSuggestions = (props) => {
   const options = props.coins.map((coin,idx) => {
-    <li key={idx}>{coin.currency}</li>
+    return <li key={idx}>{coin.currency}</li>
   })
 
   return <ul>{options}</ul>
