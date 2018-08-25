5f5b5e1ee51aafde321a5147ecba3d270aa4ff92
Edward McCarthy <efmccarthy26@gmail.com>
2018-08-25 14:48:28 -0600

    Small capitalization issues; should be Jwt not JWT

--- passport.js
+++ passport.js
@@ -1,5 +1,25 @@
 const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+const ExtractJWT = passportJWT.ExtractJwt;
+
 const LocalStrategy = require("passport-local").Strategy;
+const JWTStrategy = passportJWT.Strategy;
+
+passport.use(new JWTStrategy({
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey   : "my_super_secret"
+    },
+    function (jwtPayload, cb) {
+      return User.findOneById(jwtPayload.id)
+        .then(user => {
+          return cb(null, user);
+        })
+        .catch(err => {
+          return cb(err)
+        })
+    }
+))
 
 passport.use(new LocalStrategy({
     usernameField: "email",
