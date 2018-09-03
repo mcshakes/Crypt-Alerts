75c32b874d24bc12127c1ab0e575b78a91995092
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-03 10:39:53 -0600

    Returning the results from Promise.all call. Issues bringing it forth to component

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -12,17 +12,29 @@ const { Currency } = require("../models/currency")
 router.get("/api/coin-watchlist", checkAuth, (req, res) => {
   let userId = req.userData.userId;
 
+  Watchlist.find({userId: userId}, function(err, result) {
+    if (err) throw err;
 
-  // Watchlist
-  // .findById({})
-  // .then(user => {
-  //   // console.log(user)
-  //   return res.status(200).json(user)
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  //   res.status(500).json({ message: "Internal server error" });
-  // })
+  })
+  .then(results => {
+    return results.map(item => {
+      let coinID = item.list[0]
+
+    return Currency.findById(coinID, (err, coin) => {
+        if (err) throw err;
+      })
+      .then(coins => {
+        return coins
+      })
+    })
+  })
+  .then(promises => {
+    return Promise.all(promises)
+  })
+  .then(allCoins => {
+    // { _id: 5b8c5fca8d1e3230eaf68ea6, ticker: 'XRP', __v: 0 }
+    return allCoins
+  })
 })
 
 module.exports = router;
