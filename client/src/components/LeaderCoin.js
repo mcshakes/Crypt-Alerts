430454e8fd342ff235fbc398e2dce7efc5bc20ae
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-07 22:05:14 -0600

    Some clarity to prices

--- client/src/components/LeaderCoin.js
+++ client/src/components/LeaderCoin.js
@@ -6,7 +6,7 @@ class LeaderCoin extends React.Component {
   render() {
     return (
       <div className="leader-coin">
-        {this.props.currency} : {this.props.price}
+        {this.props.currency} : $ {this.props.price} USD
       </div>
     );
   }
