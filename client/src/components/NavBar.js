92f5a78fffad7d69a54144151515a12b1ae22a04
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-12 19:25:48 -0600

    Extricated log in info from dashboard to navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Login from "./Login";
+import { Link } from 'react-router-dom';
+import { authService } from "./AuthService"
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    authService.checkAuthentication(this.props);
+  }
+
+  isLoggedIn = () => {
+    return authService.authenticated()
+  }
+
+  render() {
+    console.log("THIS", this)
+    return (
+      <div className="menu">
+        { this.isLoggedIn() ? (
+          <Link to="/logout">
+            Logout
+          </Link>
+        ) : (
+          <Link to="/login">
+            Login
+          </Link>
+        ) }
+      </div>
+    )
+  }
+}
+
+export default NavBar;
