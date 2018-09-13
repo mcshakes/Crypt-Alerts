9fbb6c4c776a8e01ce71b11743ab49ddc54532a6
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-12 21:05:52 -0600

    Clean up and changed redirect actions within Router a bit

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Redirect } from 'react-router'
 
 import App from "./App";
 import CapLeader from "./CapLeader";
 import Login from "./Login";
 import Logout from "./Logout";
 import UserDashboard from "./UserDashboard";
-import AuthService from "./AuthService";
+import { authService } from "./AuthService";
 
 const Router = () => (
   <BrowserRouter>
@@ -14,7 +15,15 @@ const Router = () => (
       <Route exact path="/" component={App} />
       <Route exact path="/login" component={Login} />
       <Route path='/logout' component={Logout} />
-      <Route exact path="/dashboard" component={UserDashboard} />
+      {/* <Route exact path="/dashboard" component={UserDashboard} /> */}
+
+      <Route exact path="/dashboard" render={() => (
+        authService.loggedIn ? (
+          <Redirect to="/dashboard" />
+        ) : (
+          <Login />
+        )
+      )}/>
     </Switch>
   </BrowserRouter>
 )
