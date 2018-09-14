65bd6c5af71811204198de6d917592cc01c0cc74
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-14 15:29:45 -0600

    Fleshed out Register form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,48 @@
-const React = require("react")
+import React from "react";
+import FormErrors from "./FormErrors";
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      phoneNumber: ""
+    }
+  }
 
   render() {
     return(
-      <div>
-        POOP. YOU NEED TO CREATE!
+      <div className="register-form">
+        <div>
+          <FormErrors formErrors={this.state.hasError} />
+        </div>
+          <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+          <label>Phone Number</label>
+          <input
+            type="phoneNumber"
+            name="phoneNumber"
+            onChange={this.handleChange}
+            value={this.state.phoneNumber}
+          />
+          <button type="submit">
+            Register
+          </button>
+        </form>
       </div>
     )
   }
