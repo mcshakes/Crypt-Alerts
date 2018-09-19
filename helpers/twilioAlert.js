4ebf43ef7340487543500d54e59e98d7cd6dd817
Edward McCarthy <efmccarthy26@gmail.com>
2018-09-19 08:58:53 -0600

    Now sending messages via WhatsApp for price alerts

--- helpers/twilioAlert.js
+++ helpers/twilioAlert.js
@@ -5,15 +5,16 @@ const client = require("twilio")(accountSID, authToken);
 
 function createMessage(message) {
   console.log("INCOMING HIGH", message)
-  // client.messages
-  // .create({
-  //   body: "TEST!",
-  //   from: 'whatsapp:+14155238886',
-  //   to: 'whatsapp:+19707691296'
-  // })
-  // .then(message => console.log(message.sid))
-  // .catch(err => console.log(err));
-  // .done();
+
+  client.messages
+  .create({
+    body: `This is an Alert that ${message[0].ticker} is close to your price alert of $ ${message[1]} USD`,
+    from: 'whatsapp:+14155238886',
+    to: 'whatsapp:+19707691296'
+  })
+  .then(message => console.log(message.sid))
+  .catch(err => console.log(err))
+  .done();
 }
 
 module.exports = { createMessage }
