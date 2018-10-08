eb860136abfe5dfdc20c197a4e040ba15138ebfe
Edward McCarthy <efmccarthy26@gmail.com>
2018-10-08 06:37:25 -0600

    Namespaced the chart IO listener. Need to separate into per price look-ups

--- client/src/components/CurrencyChart.js
+++ client/src/components/CurrencyChart.js
@@ -2,25 +2,25 @@ import React from "react";
 import IOClient from "socket.io-client";
 
 class CurrencyChart extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     data: null,
-  //     response: false,
-  //     endpoint: "http://127.0.0.1:3001"
-  //   };
-  // }
-  //
-  // componentDidMount() {
-  //   const { endpoint } = this.state;
-  //   const socket = IOClient(endpoint);
-  //
-  //   socket.on("FromAPI", data => {
-  //     this.setState({
-  //       data: data
-  //     })
-  //   })
-  // }
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+      response: false,
+      endpoint: "/charts-io"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = IOClient(endpoint);
+
+    socket.on("FromAPI", data => {
+      this.setState({
+        data: data
+      })
+    })
+  }
 
   render() {
     return (
