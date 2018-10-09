0231c278c1b87e352aeeae72242210b1b13347d7
Edward McCarthy <efmccarthy26@gmail.com>
2018-10-09 06:41:09 -0600

    Imported all packages from Victory in top level file

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import Router from '../src/components/Router';
+import * as V from "victory";
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
