c5d39029a714381de4cf956caadd16e2ce2e176f
Edward McCarthy <efmccarthy26@gmail.com>
2018-10-14 17:23:52 -0600

    Better look for LeaderCoins

--- client/src/components/LeaderCoin.js
+++ client/src/components/LeaderCoin.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Client from "../Client";
 import '../css/CapLeader.css';
+import NumberFormat from 'react-number-format';
 
 class LeaderCoin extends React.Component {
   render() {
+    const fPrice = <NumberFormat value={this.props.price} displayType={"text"} thousandSeparator={true} prefix={'$'} decimalScale={2} />
+
     return (
       <div className="leader-coin">
-        {this.props.currency} : $ {this.props.price} USD
+        {this.props.currency} : {fPrice} USD
       </div>
     );
   }
