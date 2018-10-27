f3d2673364baecb21aac4f35b5b5093c58645a49
Edward McCarthy <efmccarthy26@gmail.com>
2018-10-27 13:07:40 -0600

    Added the graph but need better styles and placement

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import IntroSVG from "./IntroSVG"
+import '../css/LandingPage.css';
 
 class LandingPage extends React.Component {
+
+
   render() {
     return (
       <div>
-        <h1>HEY!</h1>
-        why dont you login
+        <h1>CryptWatch</h1>
+        <h2>Watch and Hodl. All in one place</h2>
+        <h5>When it comes to I've always enjoyed watching my money dwindle with the constant . Now, I can sign up and keep an eye on all the cryptocurrencies plummeting, giving me peace of mind. Better to know what you’re losing than not know.
+        </h5>
+
+        <div className="graph-image">
+          <IntroSVG />
+        </div>
+
         <Link to="/home">Login</Link>
       </div>
     )
