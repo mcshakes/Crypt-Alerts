b2ea3ccb0bfbcdae54f078863700218ebfba1d0c
Edward McCarthy <efmccarthy26@gmail.com>
2018-11-09 13:22:45 -0700

    Non-stubbed tests passing

---
+++ test/login-test.js
@@ -0,0 +1,54 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require("chai");
+const sinon = require("sinon");
+const chaiHttp = require("chai-http");
+const server = require('../server');
+const should = chai.should();
+const request = require("request");
+chai.use(chaiHttp);
+
+const base = 'http://localhost:3001';
+
+describe('Login Service', () => {
+
+  describe('when not stubbed', () => {
+
+    describe("POST /api/users/login", () => {
+
+      it("should log user in", (done) => {
+        const options = {
+          method: "post",
+          body: {
+          	"email": "test_user@test.com",
+          	"password": "password1"
+          },
+          json: true,
+          url: `${base}/api/users/login`
+        };
+
+        request.post(options, (err, res, body) => {
+          res.statusCode.should.equal(200);
+          res.headers["content-type"].should.contain("application/json");
+          body.status.should.eql("success");
+          console.log(body.data)
+        });
+        done();
+
+      }) // it block
+
+    })
+
+  });
+
+  describe('when stubbed', () => {
+    beforeEach(() => {
+      this.get = sinon.stub(request, 'post');
+    });
+    afterEach(() => {
+      request.restore();
+    });
+    // test cases
+  });
+
+});
