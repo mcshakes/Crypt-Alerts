859ff2eaa5223f2ac1b9f3e7187df12ecf4704ce
Edward McCarthy <efmccarthy26@gmail.com>
2018-11-15 20:34:49 -0700

    Some renaming, and reorganizing

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -10,36 +10,35 @@ class LandingPage extends React.Component {
 
   render() {
     return (
-      <div className="land-page">
-        <section className="main-hero">
+      <div className="landing-page">
+        
+        <div className="call-action">
           <div className="captions">
             <h1>CryptWatch</h1>
             <h1>All the Coins. One place</h1>
           </div>
 
-          <div className="explanation">
             <div className="login-aside">
               <button>
                 <Link to="/home">Login</Link>
               </button>
             </div>
-            <section className="intro-text">
-              <h5>When it comes to watching money dwindle, stay current with real time prices. Sign up and keep an eye on all the cryptocurrencies plummeting. Peace of mind means knowing what you're losing.
-              </h5>
-            </section>
-          </div>
+        </div>
 
-          <div className="illustration">
-            <img src={Dash} alt="dash img" id="dash" className="crypto-icons" />
-            <img src={Eth} alt="eth img" id="eth" className="crypto-icons" />
-            <img src={Monitor} alt="monitor img" id="monitor" />
-          </div>
-
-        </section>
+        <div className="illustration">
+          <img src={Dash} alt="dash img" id="dash" className="crypto-icons" />
+          <img src={Eth} alt="eth img" id="eth" className="crypto-icons" />
+          <img src={Monitor} alt="monitor img" id="monitor" />
+        </div>
 
         <div className="graph-image">
           <IntroSVG />
         </div>
+
+        <section className="footer-text">
+          <h5>When it comes to watching money dwindle, stay current with real time prices. Sign up and keep an eye on all the cryptocurrencies plummeting. Peace of mind means knowing what you're losing.
+          </h5>
+        </section>
 
 
       </div>
