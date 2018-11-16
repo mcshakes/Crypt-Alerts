d534dd0a3c97dc998c74d4fa70ce1c724571ceb0
Edward McCarthy <efmccarthy26@gmail.com>
2018-11-16 13:48:14 -0700

    Removed traces of store

--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createStore, combineReducers } from "redux";
-import reducers from "../../src/reducers/reducers.js";
-
-export const store = createStore(
-  combineReducers({
-    state: reducers
-  })
-);
