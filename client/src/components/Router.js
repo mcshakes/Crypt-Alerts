6308183bebe5e3da85d7f59b3c0e394df463fdd6
mcshakes <efmccarthy26@gmail.com>
2019-10-01 20:14:57 -0600

    Clean up and dashboard addition

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Redirect } from 'react-router'
 
-import LandingPage from "../landingPage/LandingPage";
+import LandingPage from "../landing-page/LandingPage";
+import Dashboard from "../user-dashboard/dashboard";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LandingPage} />
-
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   </BrowserRouter>
 )
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import Router from '../src/components/Router';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Router />,
-    document.getElementById('root')
-  );
+  <Router />,
+  document.getElementById('root')
+);
 
-registerServiceWorker();
