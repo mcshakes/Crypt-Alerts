0b0f59d0e1db73904f76c7362dfeef799e910cca
mcshakes <efmccarthy26@gmail.com>
2019-10-15 13:19:11 -0600

    Seems like handling auth state is good with the rewrite

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './css/index.css';
-import Routes from './Routes';
+import App from './App';
 
 import { Provider } from 'react-redux';
 import store from "./store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
