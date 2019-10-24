5756057f44477ea4e51b3c806689ec58f61d027f
mcshakes <efmccarthy26@gmail.com>
2019-10-24 16:42:18 -0600

    Deleted unnecessary files, and redoing home css with some variables

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./Home.css";
 
 class Home extends React.Component {
     render() {
         return (
             <div>
                 <h1>This is Home</h1>
+                <ul>
+                    <h3>Top 20 </h3>
+                </ul>
             </div>
         )
     }
