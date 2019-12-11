c82e1bb9a8ea6276c666e5f6a9f2879f566a3871
mcshakes <efmccarthy26@gmail.com>
2019-12-10 20:34:43 -0700

    Succesfully moved everything out of Home component

---
+++ client/src/components/topCryptoAssets/topCryptoAssets.js
@@ -0,0 +1,114 @@
+import React from "react";
+import IOClient from "socket.io-client";
+import "./topCryptoAssets.css";
+
+
+class TopCryptoAssets extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: null,
+            isLoading: false,
+            ioEndpoint: "/capleader"
+        }
+    }
+
+    componentDidMount() {
+        this.fetchAllMarketLeaders()
+    }
+
+    fetchAllMarketLeaders = () => {
+        this.setState({ isLoading: true })
+
+        const { ioEndpoint } = this.state;
+        const socket = IOClient(ioEndpoint);
+
+        socket.on("FromAPI", data => {
+            this.setState({
+                data: data,
+            })
+        })
+    }
+
+    renderTableHeader() {
+        let header = ["Symbol", "Name", "Price", "Market Cap", "24H Change", "7d Change ", "30d Change", "1Year Change", "All Time High"]
+
+        return header.map((key, idx) => {
+            return <th key={idx} className="table-headers">{key}</th>
+        })
+    }
+
+    checkPriceLoss = (price) => {
+        let priceElems = price.split("");
+        if (priceElems[0] === "-") {
+            return true
+        }
+    }
+
+    renderTableData = () => {
+        return this.state.data.map((coin, idx) => {
+
+            const { id, symbol, name, price, market_cap, high } = coin
+            const OneDayChange = parseFloat(coin["1d"].price_change_pct).toFixed(2);
+            const SevenDayChange = parseFloat(coin["7d"].price_change_pct).toFixed(2);
+            const ThirtyDayChange = parseFloat(coin["30d"].price_change_pct).toFixed(2);
+            const YearChange = parseFloat(coin.ytd.price_change_pct).toFixed(2);
+            const priceDollar = parseFloat(price).toFixed(2);
+            const allTimeHigh = parseFloat(high).toFixed(2);
+
+            const priceStyling = (price) => {
+                let base = "price-color-";
+                if (this.checkPriceLoss(price)) base += "loss";
+                if (!this.checkPriceLoss(price)) base += "gain"
+                return base;
+            }
+
+            return (
+                <tr className="table-row" key={id}>
+                    <td>{symbol}</td>
+                    <td>{name}</td>
+                    <td>$ {priceDollar}</td>
+                    <td>{market_cap}</td>
+                    <td className={priceStyling(OneDayChange)}>{OneDayChange}%</td>
+                    <td className={priceStyling(SevenDayChange)}>{SevenDayChange}%</td>
+                    <td className={priceStyling(ThirtyDayChange)}>{ThirtyDayChange}%</td>
+                    <td className={priceStyling(YearChange)}>{YearChange}%</td>
+                    <td>$ {allTimeHigh}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+
+        let renderObject;
+        let { isLoading } = this.state;
+        let { data } = this.state;
+
+        if (!isLoading && !data) {
+            renderObject = <span>LOADING ... </span>
+        }
+        else if (isLoading && !data) {
+            renderObject = <span>LOADING ... </span>
+        }
+        else {
+            renderObject = (
+                <table className="top-cryptos-table">
+                    <tbody>
+                        <tr>{this.renderTableHeader()}</tr>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
+            )
+        }
+
+        return (
+            <div>
+                {renderObject}
+            </div>
+        )
+    }
+}
+
+export default TopCryptoAssets;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,109 +1,34 @@
 import React from "react";
 import "./Home.css";
-import IOClient from "socket.io-client";
-import CoinItem from "../../components/MarketLeaders/CoinItem";
+import TopCryptoAssets from "../../components/topCryptoAssets/topCryptoAssets";
 
 class Home extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            data: null,
-            isLoading: false,
-            ioEndpoint: "/capleader"
-        }
-    }
 
-    componentDidMount() {
-        this.fetchAllMarketLeaders()
-    }
+    // componentDidMount() {
+    //     this.fetchAllMarketLeaders()
+    // }
 
-    fetchAllMarketLeaders = () => {
-        this.setState({ isLoading: true })
+    // fetchAllMarketLeaders = () => {
+    //     this.setState({ isLoading: true })
 
-        const { ioEndpoint } = this.state;
-        const socket = IOClient(ioEndpoint);
+    //     const { ioEndpoint } = this.state;
+    //     const socket = IOClient(ioEndpoint);
 
-        socket.on("FromAPI", data => {
-            this.setState({
-                data: data,
-            })
-        })
-    }
+    //     socket.on("FromAPI", data => {
+    //         this.setState({
+    //             data: data,
+    //         })
+    //     })
+    // }
 
-    renderTableHeader() {
-        let header = ["Symbol", "Name", "Price", "Market Cap", "24H Change", "7d Change ", "30d Change", "1Year Change", "All Time High"]
 
-        return header.map((key, idx) => {
-            return <th key={idx} className="table-headers">{key}</th>
-        })
-    }
 
-    checkPriceLoss = (price) => {
-        let priceElems = price.split("");
-        if (priceElems[0] === "-") {
-            return true
-        }
-    }
 
-    renderTableData = () => {
-        return this.state.data.map((coin, idx) => {
 
-            const { id, symbol, name, price, market_cap, high } = coin
-            const OneDayChange = parseFloat(coin["1d"].price_change_pct).toFixed(2);
-            const SevenDayChange = parseFloat(coin["7d"].price_change_pct).toFixed(2);
-            const ThirtyDayChange = parseFloat(coin["30d"].price_change_pct).toFixed(2);
-            const YearChange = parseFloat(coin.ytd.price_change_pct).toFixed(2);
-            const priceDollar = parseFloat(price).toFixed(2);
-            const allTimeHigh = parseFloat(high).toFixed(2);
 
-            const priceStyling = (price) => {
-                let base = "price-color-";
-                if (this.checkPriceLoss(price)) base += "loss";
-                if (!this.checkPriceLoss(price)) base += "gain"
-                return base;
-            }
-
-            return (
-                <tr className="table-row" key={id}>
-                    <td>{symbol}</td>
-                    <td>{name}</td>
-                    <td>$ {priceDollar}</td>
-                    <td>{market_cap}</td>
-                    <td className={priceStyling(OneDayChange)}>{OneDayChange}%</td>
-                    <td className={priceStyling(SevenDayChange)}>{SevenDayChange}%</td>
-                    <td className={priceStyling(ThirtyDayChange)}>{ThirtyDayChange}%</td>
-                    <td className={priceStyling(YearChange)}>{YearChange}%</td>
-                    <td>$ {allTimeHigh}</td>
-                </tr>
-            )
-        })
-    }
 
     render() {
-        let renderObject;
-        let { isLoading } = this.state;
-        let { data } = this.state;
 
-        // 1) it starts false and null
-        // 2) then true and null 
-        // 3) finally, true and has data 
-
-        if (!isLoading && !data) {
-            renderObject = <span>LOADING ... </span>
-        }
-        else if (isLoading && !data) {
-            renderObject = <span>LOADING ... </span>
-        }
-        else {
-            renderObject = (
-                <table className="top-cryptos-table">
-                    <tbody>
-                        <tr>{this.renderTableHeader()}</tr>
-                        {this.renderTableData()}
-                    </tbody>
-                </table>
-            )
-        }
 
 
         return (
@@ -111,8 +36,7 @@ class Home extends React.Component {
                 <h1>This is Home</h1>
                 <section className="top-cryptos-container">
                     <h3>Top 20 </h3>
-
-                    {renderObject}
+                    <TopCryptoAssets />
                 </section>
             </div>
         )
