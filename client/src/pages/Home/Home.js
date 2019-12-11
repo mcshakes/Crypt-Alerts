186a81f6a9b3304adac2e96877f9369907b9250c
mcshakes <efmccarthy26@gmail.com>
2019-12-10 20:36:03 -0700

    Clean ip

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,33 +4,7 @@ import TopCryptoAssets from "../../components/topCryptoAssets/topCryptoAssets";
 
 class Home extends React.Component {
 
-    // componentDidMount() {
-    //     this.fetchAllMarketLeaders()
-    // }
-
-    // fetchAllMarketLeaders = () => {
-    //     this.setState({ isLoading: true })
-
-    //     const { ioEndpoint } = this.state;
-    //     const socket = IOClient(ioEndpoint);
-
-    //     socket.on("FromAPI", data => {
-    //         this.setState({
-    //             data: data,
-    //         })
-    //     })
-    // }
-
-
-
-
-
-
-
     render() {
-
-
-
         return (
             <div>
                 <h1>This is Home</h1>
