deec9003f9b857c8918bae8fe9e18499cecd3dc8
mcshakes <efmccarthy26@gmail.com>
2020-02-09 07:22:34 -0700

    Rendering possible list now

---
+++ client/src/components/SearchManager/SearchCoin.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./SearchManager.css";
+
+
+class SearchCoin extends React.Component {
+
+    // addToWatchlist = () => {
+    //     let ticker = this.props.coin.currency
+    //     let price = this.props.coin.price
+    //     let token = authService.getToken();
+
+    //     const settings = {
+    //         method: "POST",
+    //         headers: {
+    //             Accept: "application/json",
+    //             "Content-Type": "application/json",
+    //             "Authorization": `Bearer ${token}`
+    //         },
+    //         body: JSON.stringify({
+    //             ticker: ticker,
+    //             price: price
+    //         })
+    //     };
+
+    //     fetch("/api/add-coin", settings)
+    //         .then((response) => {
+    //             return response.json()
+    //         })
+    //         .catch(err => {
+    //             return err
+    //         })
+    // }
+
+    clickHandler = () => {
+        // this.addToWatchlist()
+        // this.props.addNew(this.props.coin)
+    }
+
+    render() {
+        return (
+            <div className="search-results">
+                <li>
+                    <h3> {this.props.coin.currency} </h3>
+                    <button
+                        className="add-to-list"
+                        onClick={this.clickHandler}
+                    >
+                        + to Watchlist
+              </button>
+                </li>
+            </div>
+        )
+    }
+}
+
+export default SearchCoin;
