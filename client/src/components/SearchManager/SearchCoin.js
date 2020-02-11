c1228d68279444cc25145f8b26d22954af212c41
mcshakes <efmccarthy26@gmail.com>
2020-02-11 09:41:07 -0700

    Changed to a function

--- client/src/components/SearchManager/SearchCoin.js
+++ client/src/components/SearchManager/SearchCoin.js
@@ -1,58 +1,111 @@
 import React from "react";
 import "./SearchManager.css";
 
+const SearchCoin = (props) => {
+    const [token, getToken] = React.useState(
+        localStorage.getItem("token")
+    )
 
-class SearchCoin extends React.Component {
+    const addToWatchlist = () => {
+        let ticker = props.coin.currency
+        let price = props.coin.price
 
-    // addToWatchlist = () => {
-    //     let ticker = this.props.coin.currency
-    //     let price = this.props.coin.price
-    //     let token = authService.getToken();
+        console.log(token)
+        // const settings = {
+        //     method: "POST",
+        //     headers: {
+        //         Accept: "application/json",
+        //         "Content-Type": "application/json",
+        //         "Authorization": `Bearer ${token}`
+        //     },
+        //     body: JSON.stringify({
+        //         ticker: ticker,
+        //         price: price
+        //     })
+        // };
 
-    //     const settings = {
-    //         method: "POST",
-    //         headers: {
-    //             Accept: "application/json",
-    //             "Content-Type": "application/json",
-    //             "Authorization": `Bearer ${token}`
-    //         },
-    //         body: JSON.stringify({
-    //             ticker: ticker,
-    //             price: price
-    //         })
-    //     };
+        // fetch("/api/add-coin", settings)
+        //     .then((response) => {
+        //         return response.json()
+        //     })
+        //     .catch(err => {
+        //         return err
+        //     })
+    }
 
-    //     fetch("/api/add-coin", settings)
-    //         .then((response) => {
-    //             return response.json()
-    //         })
-    //         .catch(err => {
-    //             return err
-    //         })
-    // }
-
-    clickHandler = () => {
-        // this.addToWatchlist()
+    const clickHandler = () => {
+        addToWatchlist()
         // this.props.addNew(this.props.coin)
     }
 
-    render() {
-        let { price } = this.props.coin;
-        let fixedPrice = parseFloat(price).toFixed(2);
+    let { price } = props.coin;
+    let fixedPrice = parseFloat(price).toFixed(2);
 
-        return (
-            <li className="coin-result">
-                <p> {this.props.coin.currency} </p>
-                <p> {fixedPrice} </p>
-                <button
-                    className="add-to-list"
-                    onClick={this.clickHandler}
-                >
-                    + to Watchlist
+    return (
+        <li className="coin-result">
+            <p> {props.coin.currency} </p>
+            <p> {fixedPrice} </p>
+            <button
+                className="add-to-list"
+                onClick={clickHandler}
+            >
+                + to Watchlist
               </button>
-            </li>
-        )
-    }
+        </li>
+    )
 }
+
+// class SearchCoin extends React.Component {
+//     [token, getToken] = React.useState()
+
+//     addToWatchlist = () => {
+//         let ticker = this.props.coin.currency
+//         let price = this.props.coin.price
+
+//         const settings = {
+//             method: "POST",
+//             headers: {
+//                 Accept: "application/json",
+//                 "Content-Type": "application/json",
+//                 // "Authorization": `Bearer ${token}`
+//             },
+//             body: JSON.stringify({
+//                 ticker: ticker,
+//                 price: price
+//             })
+//         };
+
+//         fetch("/api/add-coin", settings)
+//             .then((response) => {
+//                 return response.json()
+//             })
+//             .catch(err => {
+//                 return err
+//             })
+//     }
+
+//     clickHandler = () => {
+//         // this.addToWatchlist()
+//         // this.props.addNew(this.props.coin)
+//     }
+
+//     render() {
+//         let { price } = this.props.coin;
+//         let fixedPrice = parseFloat(price).toFixed(2);
+
+//         return (
+//             <li className="coin-result">
+//                 <p> {this.props.coin.currency} </p>
+//                 <p> {fixedPrice} </p>
+//                 <button
+//                     className="add-to-list"
+//                     onClick={this.clickHandler}
+//                 >
+//                     + to Watchlist
+//               </button>
+//             </li>
+//         )
+//     }
+// }
 
 export default SearchCoin;
